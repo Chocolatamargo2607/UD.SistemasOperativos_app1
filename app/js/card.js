e922d8512fba33693eaf41ebdd2379a7b69eabc8
J4F3ET <jslopezz@udistrital.edu.co>
2023-04-21 17:09:20 -0500

    Agregando sistema radom de cartas

--- app/js/card.js
+++ app/js/card.js
@@ -1,4 +1,4 @@
-const cards_all = document.querySelectorAll('.card_general');
+var cards_all = document.querySelectorAll('.card_general');
 var cards_lista = document.querySelectorAll('.card_lista')
 var cards_espera = document.querySelectorAll(".card_espera");
 var card_activa = document.querySelector(".card_activa");
@@ -7,44 +7,45 @@ var container_card_proceso = document.querySelector(
 );
 var container_card_espera_1 = document.querySelector('#container_card_espera_1')
 var container_card_espera_2 = document.querySelector('#container_card_espera_2')
-function tiempoNormal(duracion){
-	const temporizador = document.getElementById("temporizador");
-	const tiempoInicio = Date.now();
-	const intervaloTemporizador = setInterval(() => {
-		const tiempoActual = Date.now();
-		const tiempoRestante = duracion * 1000 - (tiempoActual - tiempoInicio);
-		if (tiempoRestante <= 0) {
-			clearInterval(intervaloTemporizador);
-			temporizador.textContent = "Tiempo expirado";
-		} else {
-			temporizador.textContent = `${Math.floor(
-				tiempoRestante / 1000
-			)} segundos`;
-		}
-	}, 1000);
-};
+var timeInf = 0;
 function tiempoInf(){
     document.querySelectorAll(".temporizador").forEach(span => {
         span.innerHTML = '<i class="bi bi-infinity"></i>';
     });
 };
-document.querySelector('#vbtn-radio1').addEventListener('click',()=>{
+function tiempoNormal(){
+    document.querySelectorAll(".temporizador").forEach(span => {
+        span.innerHTML = '5s';
+    });
+};
+document.querySelector("#vbtn-radio1").addEventListener("click", () => {
 	//Aqui va el codigo para el boton de inicio de procesos
+	cardRandom(cards_lista)
+	Math.floor(Math.random() * cards_lista.length);
+	console.log(
+		
+	);
 });
+
 document.querySelector('#vbtn-radio2').addEventListener('click',()=>{
 	//Aqui va el codigo para el boton de pausa de procesos
 });
 document.querySelector('#vbtn-radio3').addEventListener('click',()=>{
 	//Aqui va el codigo para reiniciar el proceso
+	window.location.reload();
 });
 document.querySelector('#vbtn-radio_p_1').addEventListener('click',()=>{
 	//Aqui va el codigo de tiempo infinito
+	timeInf = 1;
+	tiempoInf();
 });
 document.querySelector('#vbtn-radio_p_2').addEventListener('click',()=>{
 	//Aqui va el codigo de todos los filososfos al mismo tiempo a comer
 });
 document.querySelector('#vbtn-radio_p_3').addEventListener('click',()=>{
 	//Aqui va el codigo de tiempo normal
+	timeInf = 0;
+	tiempoNormal();
 });
 cards_lista.forEach((card) => {
 	card.addEventListener("click", () => {
@@ -57,12 +58,15 @@ cards_lista.forEach((card) => {
 			//No hay elementos en el area critica
 			areaCriticaLibre(card);
 		}
+		actualizarVarCards();
 	});
 });
 function areaCriticaEnUso(card) {
 	//Aqui va el codigo para cuando el area critica este en uso
 	card = arreglandoCardEspera(card);
-	iniciarTemporizadorEspera(card);
+	if(timeInf == 0){
+		iniciarTemporizadorEspera(card);
+	}
 	if (container_card_espera_1.children.length < 3) {
 		//Card se agrega al lista de espera
 		container_card_espera_1.appendChild(card);
@@ -70,17 +74,20 @@ function areaCriticaEnUso(card) {
 		//Card se agrega al lista de espera
 		container_card_espera_2.appendChild(card);
 	}
-	console.log(container_card_espera_1.children.length);
 }
 function areaCriticaLibre(card) {
 	//Aqui va el codigo para cuando el area critica este libre
 	card = arreglandoCardActiva(card);
-	iniciarTemporizadorActivo(card);
+	if(timeInf == 0){
+		iniciarTemporizadorActivo(card);
+	}
 	container_card_proceso.appendChild(card);
 }
 function arreglandoCardActiva(card) {
 	card.classList.add("card_activa");
 	img = card.querySelector("img");
+	card.querySelector(".td_estado").innerHTML = "Comiendo...";
+	card.querySelector(".td_proceso").innerHTML = "Corriendo...";
 	temporizador = card.querySelector(".temporizador");
 	temporizador.classList.add("temporizador_activa");
 	if (card.classList.contains("card_espera")) {
@@ -113,6 +120,8 @@ function iniciarTemporizadorActivo(card) {
 function arreglandoCardEspera(card){
 	card.classList.add("card_espera");
 	img = card.querySelector("img");
+	card.querySelector(".td_estado").innerHTML = "Esperando...";
+	card.querySelector(".td_proceso").innerHTML = "Hambriento";
 	temporizador= card.querySelector(".temporizador");
 	temporizador.classList.add("temporizador_espera");
 	if (card.classList.contains("card_activa")) {
@@ -141,4 +150,36 @@ function iniciarTemporizadorEspera(card) {
 			// Código a ejecutar al finalizar el temporizador
 		}
 	}, 1000);
+}
+function cardRandom(cards) {
+	let count = 0;
+	const intervalId = setInterval(() => {
+		const prevCard = cards[(count - 1 + cards.length) % cards.length];
+		const currCard = cards[count % cards.length];
+		prevCard.classList.remove("card_random");
+		currCard.classList.add("card_random");
+		count++;
+		if (count >= cards.length * 3) {
+			clearInterval(intervalId);
+			currCard.classList.remove("card_random");
+			document
+				.querySelector(`#card_${Math.floor(Math.random() * cards.length)}`)
+				.click();
+		}
+	}, 400);
+}
+function actualizarVarCards() {
+	cards_all = document.querySelectorAll(".card_general");
+	 cards_lista = document.querySelectorAll(".card_lista");
+	 cards_espera = document.querySelectorAll(".card_espera");
+	 card_activa = document.querySelector(".card_activa");
+	 container_card_proceso = document.querySelector(
+		"#div_container_card_lista"
+	);
+	 container_card_espera_1 = document.querySelector(
+		"#container_card_espera_1"
+	);
+	 container_card_espera_2 = document.querySelector(
+		"#container_card_espera_2"
+	);
 }
